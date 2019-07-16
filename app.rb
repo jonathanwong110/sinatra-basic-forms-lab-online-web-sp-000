c626beff5a0b66c080a213a8a093392f02aa4c21
Jonathan Wong <jonathan.wong110@gmail.com>
2019-07-16 21:43:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ class App < Sinatra::Base
   post '/puppy' do
     @name = params[:name]
     @breed = params[:breed]
-    @months_old = params[:age]
+    @age = params[:months_old]
     erb :display_puppy
   end
   
